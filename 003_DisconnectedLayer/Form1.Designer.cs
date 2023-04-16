a15498964614384c4a77435f73137c0f87b4e86a
Viktor Velychko <Cezn.Vi@gmail.com>
2023-04-16 16:07:21 +0300

    WIP

---
+++ 003_DisconnectedLayer/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace _003_DisconnectedLayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Query = new System.Windows.Forms.TextBox();
+            this.dataGridView_Results = new System.Windows.Forms.DataGridView();
+            this.button_Execute = new System.Windows.Forms.Button();
+            this.button_ExecDataSet = new System.Windows.Forms.Button();
+            this.comboBox_selectDB = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Results)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_Query
+            // 
+            this.textBox_Query.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Query.Location = new System.Drawing.Point(13, 13);
+            this.textBox_Query.Multiline = true;
+            this.textBox_Query.Name = "textBox_Query";
+            this.textBox_Query.Size = new System.Drawing.Size(664, 83);
+            this.textBox_Query.TabIndex = 0;
+            // 
+            // dataGridView_Results
+            // 
+            this.dataGridView_Results.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_Results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Results.Location = new System.Drawing.Point(13, 103);
+            this.dataGridView_Results.Name = "dataGridView_Results";
+            this.dataGridView_Results.ReadOnly = true;
+            this.dataGridView_Results.Size = new System.Drawing.Size(664, 335);
+            this.dataGridView_Results.TabIndex = 1;
+            // 
+            // button_Execute
+            // 
+            this.button_Execute.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Execute.Location = new System.Drawing.Point(684, 103);
+            this.button_Execute.Name = "button_Execute";
+            this.button_Execute.Size = new System.Drawing.Size(135, 63);
+            this.button_Execute.TabIndex = 2;
+            this.button_Execute.Text = "Fill DataTable";
+            this.button_Execute.UseVisualStyleBackColor = true;
+            this.button_Execute.Click += new System.EventHandler(this.button_Execute_Click);
+            // 
+            // button_ExecDataSet
+            // 
+            this.button_ExecDataSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ExecDataSet.Location = new System.Drawing.Point(684, 172);
+            this.button_ExecDataSet.Name = "button_ExecDataSet";
+            this.button_ExecDataSet.Size = new System.Drawing.Size(135, 64);
+            this.button_ExecDataSet.TabIndex = 3;
+            this.button_ExecDataSet.Text = "Fill DataSet";
+            this.button_ExecDataSet.UseVisualStyleBackColor = true;
+            this.button_ExecDataSet.Click += new System.EventHandler(this.button_ExecDataSet_Click);
+            // 
+            // comboBox_selectDB
+            // 
+            this.comboBox_selectDB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBox_selectDB.FormattingEnabled = true;
+            this.comboBox_selectDB.Location = new System.Drawing.Point(684, 13);
+            this.comboBox_selectDB.Name = "comboBox_selectDB";
+            this.comboBox_selectDB.Size = new System.Drawing.Size(135, 28);
+            this.comboBox_selectDB.TabIndex = 4;
+            this.comboBox_selectDB.Text = "Select table";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 450);
+            this.Controls.Add(this.comboBox_selectDB);
+            this.Controls.Add(this.button_ExecDataSet);
+            this.Controls.Add(this.button_Execute);
+            this.Controls.Add(this.dataGridView_Results);
+            this.Controls.Add(this.textBox_Query);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DiconnectedLayer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Results)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Query;
+        private System.Windows.Forms.DataGridView dataGridView_Results;
+        private System.Windows.Forms.Button button_Execute;
+        private System.Windows.Forms.Button button_ExecDataSet;
+        private System.Windows.Forms.ComboBox comboBox_selectDB;
+    }
+}
+
---
+++ 003_DisconnectedLayer/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _003_DisconnectedLayer
+{
+    public partial class Form1 : Form
+    {
+        private string _dPName;
+        private string _connectionString;
+
+
+
+        private DbProviderFactory _dbProviderFactory;
+        private DbConnection _dbConnection;
+        private DbDataAdapter _dataAdapter;
+
+        private DataTable _dataTable;
+        private DataSet _dataSet;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _dPName = ConfigurationManager.AppSettings["SQLProvider"];
+            _connectionString = ConfigurationManager.ConnectionStrings["SqlProvider"].ConnectionString;
+
+            _dbProviderFactory = DbProviderFactories.GetFactory(_dPName);
+
+            _dbConnection = _dbProviderFactory.CreateConnection();
+            _dbConnection.ConnectionString = _connectionString;
+
+            _dataAdapter = _dbProviderFactory.CreateDataAdapter();
+
+            _dataSet = new DataSet();
+
+            GetTableNameIntoCombobox();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Execute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = textBox_Query.Text;
+                if(query.Length < 12)
+                {
+                    throw new Exception("Введите тело запроса");
+                    return;
+                }
+                _dataTable = new DataTable();
+
+                DbCommand dbCommand = _dbProviderFactory.CreateCommand();
+                dbCommand.Connection = _dbConnection;
+                dbCommand.CommandText = query;
+                _dbConnection.Open();
+
+                DbDataReader dbDataReader = dbCommand.ExecuteReader();
+
+                //Формируем DataTable------------------------------------------------start
+                int lineIndex = 0; 
+                do
+                {
+                    while (dbDataReader.Read())
+                    {
+                        if(lineIndex == 0)          //выгружаю шапку таблицы
+                        {
+                            for (int i = 0; i < dbDataReader.FieldCount; i++)
+                            {
+                                _dataTable.Columns.Add(dbDataReader.GetName(i));
+                            }
+                            lineIndex++;
+                        }
+                        DataRow dataRow = _dataTable.NewRow();
+                        for (int i = 0; i < dbDataReader.FieldCount; i++)
+                        {
+                            dataRow[i] = dbDataReader[i];
+                        }
+                        _dataTable.Rows.Add(dataRow);
+                    }
+
+                } while (dbDataReader.NextResult());
+                //Формируем DataTable------------------------------------------------end
+                dataGridView_Results.DataSource = _dataTable;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                _dbConnection.Close();
+            }
+        }
+
+        private void button_ExecDataSet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = textBox_Query.Text;
+                if (query.Length < 12)
+                {
+                    throw new Exception("Введите тело запроса");
+                    return;
+                }
+                _dataSet.Clear();
+                DbCommand dbCommand = _dbProviderFactory.CreateCommand();
+                dbCommand.Connection = _dbConnection;
+                dbCommand.CommandText = query;
+
+                _dataAdapter.SelectCommand = dbCommand;
+
+                //_dataAdapter.SelectCommand
+                //_dataAdapter.InsertCommand
+                //_dataAdapter.UpdateCommand
+                //_dataAdapter.DeleteCommand
+                _dataAdapter.Fill(_dataSet);
+
+                dataGridView_Results.DataSource = _dataSet.Tables["users"];
+
+                dataGridView_Results.DataSource = _dataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                _dbConnection.Close();
+            }
+        }
+
+
+        private void GetTableNameIntoCombobox()
+        {
+            DbCommand dbCommand = _dbProviderFactory.CreateCommand();
+            dbCommand.Connection = _dbConnection;
+            dbCommand.CommandText = "SELECT * FROM INFORMATION_SCHEMA.TABLES";
+
+            _dbConnection.Open();
+            DbDataReader dbDataReader;
+
+            using (dbDataReader = dbCommand.ExecuteReader())
+            {
+                while (dbDataReader.Read())
+                {
+                    if(dbDataReader["TABLE_NAME"].ToString() != "sysdiagrams")
+                        comboBox_selectDB.Items.Add(dbDataReader["TABLE_NAME"].ToString());
+                }
+            }
+            _dbConnection.Close();
+
+        }
+    }
+}
